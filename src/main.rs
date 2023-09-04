08dd033f61bbf1b348912d4bf344a26318a1500c
Spencer Neukam <neukams@oregonstate.edu>
2023-09-04 12:47:04 -0400

    Practiced making a loop that executed for 1 billion or even 1 trillion iterations. As the loop got larger, the non release build took longer and longer. However, in release mode, the loop executed instantly, even for 1 trillion iterations. I'm not sure the loop executing is really happening in release mode. I would imagine it would take at least a second. The processor doesn't even work that quickly. Even though the loop is time via std::time::Instant::now(), release mode probably recognizes that nothing is actually happening in the loop where we need to actually execute it for. I.e. no variable assignments come out of it, the loop doesn't actually do anything. Even though it's time, release mode probably ignores that need and optimizes by removing the loop entirely. My first thought.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+
+use std::time::Instant;
+
+/**
+ * More doc types??
+ */
 fn main() {
     println!("Hello, world!");
+
+    let x: i32 = 5;
+
+    let y = if x == 5 {5} else {-1};
+    println!("y = {y}");
+
+    billion_iterations();
+}
+
+/**
+ * Run a loop for a billion iterations. Return None.
+ */
+fn billion_iterations() {
+
+    let now = Instant::now();
+    let mut z: u64 = 0;
+
+     loop {
+        z += 1;
+        if z == 1_000_000_000_000 {
+            let elapsed = now.elapsed().as_secs();
+            println!("1 billion!");
+            println!("time elapsed = {elapsed} secs");
+            break;
+        }        
+    }
 }
